a91dd045cfb851f709143999101385d6a3c25fe2
DurgeshBarve <109893561+DurgeshBarve@users.noreply.github.com>
2022-12-27 17:15:26 +0530

    updated for version Durgesh_01

---
+++ internal/cmd/monitor.go
@@ -0,0 +1,21 @@
+package cmd
+
+import (
+	monitorUI "github.com/DurgeshBarve/gitlab-ci-pipelines-exporter/pkg/monitor/ui"
+	"github.com/urfave/cli/v2"
+)
+
+// Monitor ..
+func Monitor(ctx *cli.Context) (int, error) {
+	cfg, err := parseGlobalFlags(ctx)
+	if err != nil {
+		return 1, err
+	}
+
+	monitorUI.Start(
+		ctx.App.Version,
+		cfg.InternalMonitoringListenerAddress,
+	)
+
+	return 0, nil
+}
